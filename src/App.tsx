c6b4ec31e887da3d748373124a64e106edb44a00
junji oishi <junji.oishi@nijibox.co.jp>
2022-10-03 17:33:44 +0900

    react-test-typescript
    コンポーネントでtypescriptを使用

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import { Practice1 } from './practices/Practice1';
-import { Practice2 } from './practices/Practice2';
-import { Practice3 } from './practices/Practice3';
-import { Practice4 } from './practices/Practice4';
+import { useState } from 'react';
+import axios from 'axios';
+import { Todo } from './Todo';
 
 function App() {
+  const [todos, setTodos] = useState<any>([]);
+
+  const onClickFetchData = () => {
+    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
+      setTodos(res.data);
+    })
+  }
   return (
     <div className="App">
-      <Practice1 />
-      <Practice2 />
-      <Practice3 />
-      <Practice4 />
+      <button onClick={onClickFetchData}>データ取得</button>
+      {todos.map((todo) => {
+        return <Todo title={todo.title} userid={todo.userid} />
+      })}
     </div>
   );
 }
