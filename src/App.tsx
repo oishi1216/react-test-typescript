706a2f26a3274816f25e03d08233e0d7300fed98
junji oishi <junji.oishi@nijibox.co.jp>
2022-10-05 15:26:53 +0900

    react-test-typescript
    オプショナルチェイニングの追加

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,13 @@ import axios from 'axios';
 import { Todo } from './Todo';
 import { TodoType } from "./types/todo"
 import { Text } from './Text'
+import { UserProfile } from './UserProfile';
+import { User } from './types/user';
+
+const user: User = {
+  name: "名無し",
+  hobbies: ["映画", "ゲーム"]
+}
 
 function App() {
   const [todos, setTodos] = useState<Array<TodoType>>([]);
@@ -14,6 +21,7 @@ function App() {
   }
   return (
     <div className="App">
+      <UserProfile user={user} />
       <Text color="red" fontSize='18px' />
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todo) => {
